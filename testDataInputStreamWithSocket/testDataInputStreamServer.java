5c601b4beb8b9d49c8ddfe877da35d2262074273
Andy <673536904@qq.com>
2015-01-05 12:27:14 -0500

    Create testDataInputStreamServer.java

---
+++ testDataInputStreamWithSocket/testDataInputStreamServer.java
@@ -0,0 +1,38 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class testDataInputStreamServer {
+	static Socket sock=null;
+	static ServerSocket client=null;
+	static int port=5555;
+	
+	static DataInputStream dis=null;
+	static DataOutputStream dos=null;
+	
+	public static void main(String[] args) throws IOException
+	{
+		try {
+			client=new ServerSocket(port);
+			sock=client.accept();
+			
+			dos=new DataOutputStream(sock.getOutputStream());
+			String str="test from server";
+			dos.writeUTF(str);//it works
+			dos.flush();
+			
+		} catch (IOException e) {
+			System.out.println("can not start...");
+		}
+		finally
+		{
+			if(sock!=null)sock.close();
+			if(client!=null)client.close();
+		}
+	}
+}
+//It can now send a String to the client
+//future work: readsomething
